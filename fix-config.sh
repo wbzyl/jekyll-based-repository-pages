071306e56340a9fd61eb7d7a3eaafd8b373ebec1
Wlodek Bzyl <matwb@univ.gda.pl>
2013-07-13 20:02:16 +0200

    fix-config.sh

--- fix-config.sh
+++ fix-config.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 
-# ./fix-config.sh blog/_config.yml xxl 'XXL Blog'
+# ./fix-config.sh 'XXX Blog' /xxx/ blog/_config.yml
 
-sed '
-$ a\
-\
-markdown: kramdown\
-\
-kramdown:\
-  use_coderay: true\
-  coderay:\
-    coderay_wrap: div\
-    coderay_line_numbers: nil\
-    coderay_tab_width: 2\
-    coderay_css: class\
-\
-relative_permalinks: false\
-\
-permalink: /articles/:year-:month-:day-:title.html\
-\
-baseurl: /$2/
+blog_name=${1:-My Awesome Blog}
+repo_name=${2:-/}
+config_file=${3:-blog/_config.yml}
 
-s/name: .*$/name: $3/
-/pygments: true/d
-' $1
+sed "
+/name/ c\\
+name=${blog_name}
+
+$ a\\
+\\
+markdown: kramdown\\
+\\
+kramdown:\\
+  use_coderay: true\\
+  coderay:\\
+    coderay_wrap: div\\
+    coderay_line_numbers: nil\\
+    coderay_tab_width: 2\\
+    coderay_css: class\\
+\\
+relative_permalinks: false\\
+permalink: /articles/:year-:month-:day-:title.html\\
+\\
+baseurl: ${repo_name}
+
+/pygments: true/ d
+" $config_file
+
+
