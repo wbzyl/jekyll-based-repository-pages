a65c8cd7a8e594f508ebb9b98ec4cbe79411b572
Wlodek Bzyl <matwb@univ.gda.pl>
2013-10-04 14:50:48 +0200

    poprawki w skrypcie: fix-names.sh

--- blog/fix-names.sh
+++ blog/fix-names.sh
@@ -2,7 +2,7 @@
 
 # run this from the blog/ directory
 #
-#   ./fix-config.sh /xxx 'My Awesome XXX Blog'
+#   ./fix-config.sh /abc 'Blogging ABC'
 
 git_repo_name=${1:-}
 blog_name=${2:-Your New Jekyll Site}
@@ -14,3 +14,8 @@ name: ${blog_name}
 /baseurl:/ c\\
 baseurl: ${git_repo_name}
 " _config.yml
+
+sed -i "
+/h1 class=\"title\"/ c\\
+          <h1 class=\"title\"><a href=\"${git_repo_name}/\">{{ site.name }}</a></h1>
+" _layouts/default.html
